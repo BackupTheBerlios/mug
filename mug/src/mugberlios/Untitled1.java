a9540a16c9f5a2d32a516cd9af04f94c128f87a2
dpichugin <dpichugin>
2005-03-02 08:20:34 +0000

    Initial revision

---
+++ mug/src/mugberlios/Untitled1.java
@@ -0,0 +1,18 @@
+package mugberlios;
+
+/**
+ * <p>Title: </p>
+ *
+ * <p>Description: </p>
+ *
+ * <p>Copyright: Copyright (c) 2005</p>
+ *
+ * <p>Company: </p>
+ *
+ * @author not attributable
+ * @version 1.0
+ */
+public class Untitled1 {
+    public Untitled1() {
+    }
+}
